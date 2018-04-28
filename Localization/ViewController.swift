99af90f43828e7e99d9ae44a09e7e1e021d507bb
Yauheni Baranouski <yauheni_baranouski@epam.com>
2018-04-28 11:20:23 +0300

    init commit

---
+++ Localization/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Localization
+//
+//  Created by Yauheni Baranouski on 4/28/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
